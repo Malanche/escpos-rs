9bd966e9d55e016bd280ad297610cf248c4d49eb
Carlos Malanche <carlos.malanche.f@gmail.com>
2021-08-08 13:30:12 -0500

    Deleted another commented block

--- src/instruction/mod.rs
+++ src/instruction/mod.rs
@@ -7,10 +7,3 @@ mod instruction;
 mod print_data;
 mod justification;
 mod escpos_image;
-
-/*
-let img = match image::load_from_memory(&content) {
-    Ok(i) => i,
-    Err(e) => return Err(Error::ImageError(e))
-};
-*/
